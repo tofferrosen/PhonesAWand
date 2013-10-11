403f568d638294f998968a35f400a0feaf717e1e
Christoffer Rosen <cbr4830@rit.edu>
2013-10-11 15:39:11 -0400

    Added stubbed out methods that respond to when the button is pressed down and released.

--- src/rit/se356/phonesawand/MainActivity.java
+++ src/rit/se356/phonesawand/MainActivity.java
@@ -1,8 +1,13 @@
 package rit.se356.phonesawand;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button doMagic = (Button) findViewById(R.id.perform_magic);
+		doMagic.setOnTouchListener(new View.OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                	startRecordingMagic();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                	stopRecordingMagic();
+                return false;
+			}
+		});
 	}
 
 	@Override
@@ -17,6 +34,24 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/**
+	 * startRecordingMagic
+	 * Called when the Cast Spell button is pressed down.
+	 * Start recording user's motion and voice.
+	 */
+	public void startRecordingMagic() {
+		Log.d("Pressed", "Start Recording Magic!");
+	}
+	
+	/**
+	 * stopRecordingMagic
+	 * Called when the Cast Spell button is released
+	 * Stop recording the user's motion and voice.
+	 */
+	public void stopRecordingMagic() {
+		Log.d("Released", "Stop Recording Magic!");
 	}
 
 }
