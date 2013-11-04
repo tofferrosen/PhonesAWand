0955bf035a10afa4505b46d71738f9621704b6f5
Christoffer Rosen <cbr4830@rit.edu>
2013-11-04 14:48:09 -0500

    Using dummy data for spells in grimoire

--- gen/rit/se356/phonesawand/R.java
+++ gen/rit/se356/phonesawand/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int roundbackground=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int goto_grimoire=0x7f080000;
-        public static final int perform_magic=0x7f080001;
+        public static final int action_settings=0x7f080003;
+        public static final int goto_grimoire=0x7f080001;
+        public static final int listview=0x7f080000;
+        public static final int perform_magic=0x7f080002;
     }
     public static final class layout {
         public static final int activity_grimoire=0x7f030000;
