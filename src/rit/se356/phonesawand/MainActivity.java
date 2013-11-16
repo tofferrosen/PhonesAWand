e14a6c34b085a80e534e540a31b16ecc2bdac134
Christoffer Rosen <cbr4830@rit.edu>
2013-11-15 20:44:36 -0500

    Refactored voice into seperate class (or all into the speech listener. Fixed the funky background of cast button. Voice when results are received now calls a method in main activity with the string results as a parameter.

--- src/rit/se356/phonesawand/MainActivity.java
+++ src/rit/se356/phonesawand/MainActivity.java
@@ -17,13 +17,11 @@ import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 
 public class MainActivity extends Activity {
 
 	protected static final int RESULT_SPEECH = 1;
-	private SpeechRecognizer speech;
+	private SpeechListener listener = null;
 	private SensorManager mSensorManager;
 	private Sensor mSensor;
 	
@@ -48,7 +46,7 @@ public class MainActivity extends Activity {
                 if (event.getAction() == MotionEvent.ACTION_DOWN)
                 	startRecordingMagic();
                 else if (event.getAction() == MotionEvent.ACTION_UP)
-                	stopRecordingMagic();
+					stopRecordingMagic();
                 return false;
 			}
 		});
@@ -62,6 +60,15 @@ public class MainActivity extends Activity {
 	}
 	
 	/**
+	 * This method is called by the SpeechListener when done listening for voice
+	 * @param voiceResult			string of the voice results captured by SpeechListener
+	 */
+	public void voiceFinished(String voiceResult){
+		Log.d("MAIN: ", voiceResult);
+	}
+	
+	
+	/**
 	 * startRecordingMagic
 	 * Called when the Cast Spell button is pressed down.
 	 * Start recording user's motion and voice.
@@ -72,28 +79,14 @@ public class MainActivity extends Activity {
 		startRecordingMotion();
 	}
 
+	/**
+	 * Starts recording voice.
+	 */
 	public void startRecordingVoice() {
-		
-		SpeechListener listener = new SpeechListener();
-		
-		speech = SpeechRecognizer.createSpeechRecognizer(this);
-		speech.setRecognitionListener(listener);
-	
-		
-		Intent intent = new Intent(
-				RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-		intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
-		        this.getPackageName());
-		
-		try {
-			speech.startListening(intent);
-		} catch (ActivityNotFoundException a) {
-			// device doesn't support speech to text
-			Log.d("Error:", "listening to voice failed");
-		}
+		listener = new SpeechListener(this.getApplicationContext(), this); 
+		listener.startRecordingVoice();
 	}
-	
+
 	public void startRecordingMotion() {
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
@@ -110,12 +103,13 @@ public class MainActivity extends Activity {
 	 * stopRecordingMagic
 	 * Called when the Cast Spell button is released
 	 * Stop recording the user's motion and voice.
+	 * @throws InterruptedException 
 	 */
 	public void stopRecordingMagic() {
 		Log.d("Released", "Stop Recording Magic!");
 		
 		// stop listening to voice commands
-		speech.stopListening();
+		listener.stopRecordingVoice();
 	}
 	
 	/**
--- src/rit/se356/phonesawand/SpeechListener.java
+++ src/rit/se356/phonesawand/SpeechListener.java
@@ -3,17 +3,73 @@ package rit.se356.phonesawand;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
 
 /**
- * Implements the Android RecognitionListner
+ * Wrapper around the Android RecognitionListner to enable recording the user's voice.
+ * 
+ * Purpose: Avoid the voice interface pop up provided by default voice listener
+ * Function: Can start and stop recording voice, returning a String of the voice
+ * 			 recorded
+ * 
  * @author toffer
  *
  */
-public class SpeechListener extends Activity implements RecognitionListener{
+public class SpeechListener implements RecognitionListener {
+	
+	private SpeechRecognizer speech = null;		/* Android Speech Recognizer */
+	private Context context = null;				/* Context of calling activity */
+	MainActivity mainActivity = null;			/* Main Activity to call voiceFinished method when 
+													recognitionlistener finishes */
+	
+	public SpeechListener(Context c, MainActivity mainActivity){
+		this.context = c;
+		this.mainActivity = mainActivity;
+	}
+	
+	/** 
+	 * Start recording the user's voice
+	 * DOES NOT STOP VOICE until stopRecordingVoice is called
+	 */
+	public void startRecordingVoice(){
+		
+		speech = SpeechRecognizer.createSpeechRecognizer(context);
+		speech.setRecognitionListener(this);
+		
+		Intent intent = new Intent(
+				RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+		//intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+		  //      this.getPackageName());
+		
+		try {
+			speech.startListening(intent); /* Start listening to user */
+		} catch (ActivityNotFoundException a) {
+			// device doesn't support speech to text
+			Log.d("Error:", "listening to voice failed");
+		}
+	}
+	
+	
+	/** 
+	 * Stops recording voice 
+	 * @return			String of the voice captured
+	 */
+	public void stopRecordingVoice(){
+		speech.stopListening();
+	}
+	
+	/* 
+	 * Many not implemented, but required to wrap the Android RecognitionListener.
+	 * Only onReceive implemented		
+	 */
 	
 	@Override
 	public void onBeginningOfSpeech() {
@@ -57,14 +113,20 @@ public class SpeechListener extends Activity implements RecognitionListener{
 		
 	}
 
-	@Override
+	/**
+	 * Called when the Recognition Listener has received results.
+	 * Calls voiceFinished method in the main activity with the voice 
+	 * results as a string parameter.
+	 */
 	public void onResults(Bundle data) {
 		ArrayList<String> magicWords = data.getStringArrayList(
 				SpeechRecognizer.RESULTS_RECOGNITION);
-
-		Log.d(" on results: ", magicWords.get(0));
 		
+		Log.d("SpeechListener results: ", magicWords.get(0));
+		String voiceResults = magicWords.get(0);
+		mainActivity.voiceFinished(voiceResults);
 	}
+
 
 	@Override
 	public void onRmsChanged(float arg0) {
